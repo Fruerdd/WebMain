6fcc35311c21c723f67c64f67e3a47b76137fc05
gusicamina <amina.gusic@stu.ssst.edu.ba>
2024-05-09 11:35:05 +0200

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transparent Login Form</title>
+    <title>Transparent Login Form!</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand:100,300,500">
     <link rel="stylesheet" href="css/styleform.css">
 </head>
